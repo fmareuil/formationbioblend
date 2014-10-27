469141444d392458fa77faebea454a1ead488a31
fmareuil <fmareuil@fee.sis.pasteur.fr>
2014-10-27 16:28:08 +0100

    debut du code pour les workflows

--- premierbioblend.py
+++ premierbioblend.py
@@ -25,9 +25,13 @@ def liste_historyfiles(key, galaxyurl, namehisto):
     """
     lists the files in a history
     """
-    gi = connectgalaxy(key, galaxyurl)
+    gi = connectgalaxy(key, url)
     filesdico = {}
-    histolist = gi.histories.get_histories()
+    histolist = gi.histories.get_histories(name="namehisto")
+    if len(histolist) == 0:
+        raise ValueError("Il n'y a pas d'historique qui porte ce nom")
+    elif len(histolist) > 1:
+        print "Il y a plus d'un historique qui porte ce nom"
     for histo in histolist:
         if histo["name"] == namehisto:
             idhisto = histo["id"]
@@ -35,6 +39,15 @@ def liste_historyfiles(key, galaxyurl, namehisto):
     for iddata in liste_data:
         filesdico[gi.datasets.show_dataset(iddata)['id']] = gi.datasets.show_dataset(iddata)['name']
     return filesdico, idhisto
+
+def run_workflow(key, url, name):
+    
+    gi = connectgalaxy(key, url)
+    workflow = gi.workflows.get_workflows(name=name)
+    if len(workflow) > 1:
+        raise ValueError("Il y a plusieurs workflows qui portent ce nom")
+    elif len(workflow) == 0:
+        raise ValueError("Pas de Workflow avec ce nom") 
 
 def specific_download_dataset(gi, dataset, id, idhisto, key, file_path, use_default_filename=True, verify=True):
     download_url = 'api/histories/' + idhisto+ '/contents/' + id + '/display?to_ext=' + dataset['file_ext'] +'&hda_ldda=' + dataset['hda_ldda'] + '&key=' + key
@@ -76,8 +89,7 @@ if __name__ == "__main__":
     parser.add_argument("-m", "--my_histories", help="list yours histories")
     parser.add_argument("-hi", "--history_list", help="list files of a history")
     parser.add_argument("-n", "--name", action="store", help="name of galaxy history")
-    parser.add_argument("-r", "--run_workflow", help="run a workflow")
-    parser.add_argument("-w", "--id_workflow", action="store", help="id of your workflow")
+    parser.add_argument("-w", "--name_workflow", action="store", help="id of your workflow")
     parser.add_argument("-d", "--download_files", help="download files")
     parser.add_argument("-f", "--filesid", nargs="+", help="file id to download")
     parser.add_argument("-p", "--path", action="store", help="output file to download")
@@ -94,8 +106,8 @@ if __name__ == "__main__":
                 print files[file], file
         else:
             print "the -h option need a history name"
-    elif args.run_workflow:
-        pass
+    elif args.name_workflow:
+        run_workflow(args.key, args.url, args.name_workflow)
     elif args.download_files:
         if args.fileid:
             downloadfile(args.key, args.url, args.fileid, idhisto, args.path)
