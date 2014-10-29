ab57db4b124e55c8994869b064eec81dab1f2cf1
Olivia Doppelt-Azeroual <olivia.doppelt@pasteur.fr>
2014-10-29 17:08:46 +0100

    degub the run_wf part; not finalized though. Need ipython to test; will do that tomorow.

--- premierbioblend.py
+++ premierbioblend.py
@@ -29,7 +29,7 @@ def _find_history(gi, namehisto):
     if len(histolist) == 0:
         raise ValueError("Il n'y a pas d'historique qui porte ce nom")
     elif len(histolist) > 1:
-        raise ValueError("Il y a plus d'un historique qui porte ce nom") 
+        raise ValueError("Il y a plus d'un historique qui porte ce nom")
     return histolist[0]
 
 def liste_historyfiles(apikey, galaxyurl, namehisto):
@@ -40,31 +40,39 @@ def liste_historyfiles(apikey, galaxyurl, namehisto):
     filesdico = {}
     histo = _find_history(gi, namehisto)
     idhisto = histo["id"]
-    liste_data = gi.histories.show_history(histo["id"],contents=False)["state_ids"]["ok"]
+    liste_data = gi.histories.show_history(histo["id"], contents=False)["state_ids"]["ok"]
     for iddata in liste_data:
         filesdico[gi.datasets.show_dataset(iddata)['id']] = gi.datasets.show_dataset(iddata)['name']
     return filesdico, idhisto
 
-def run_workflow(key, url, nameworkflow, namehistory, datasets_id):
-    gi = connectgalaxy(key, url)
-    workflow = _find_workflow(gi, nameworkflow)
-    history = _find_history(gi, namehistory)
-    #print inputs().set("input", dataset('datasets_id[0]'))
-    print "TODO: Je ne sais pas comment fait ici a regarder de plus pres"
-    datasetmap = {'4': {'id': datasets_id[0], 'src' : 'hda'}}
-    datasetmap =  dataset('datasets_id[0]').__dict__
-    gi.workflows.run_workflow(workflow['id'], history_id=history['id'], dataset_map=datasetmap)
-    
+
+def run_workflow(api_key, galaxy_url, name_wf, name_hist):#, dataset_id):
+    dataset_map = {}
+    gi = connectgalaxy(api_key, galaxy_url)
+    workflow = _find_workflow(gi, name_wf)
+    history = _find_history(gi, name_hist)
+    dataset_id = gi.histories.show_matching_datasets(history[u'id'])[0]['id']
+    print workflow[u'inputs'].keys()[0]
+    print dataset_id
+    dataset_map[workflow[u'inputs'].keys()[0]] = {'id': dataset_id, 'src': 'ld'}
+    print dataset_map
+
+    gi.workflows.run_workflow(workflow['id'], history_id=history['id'], dataset_map=dataset_map)
+    return 0
+
+#run_workflow(args.api_key, args.galaxy_url, args.name_workflow, args.hist_name, args.filesid)
+
 
 def _find_workflow(gi, name):
-
-    workflow = gi.workflows.get_workflows(name=name)
-    if len(workflow) > 1:
+    workflows = gi.workflows.get_workflows(name=name)
+    if len(workflows) > 1:
         raise ValueError("Il y a plusieurs workflows qui portent ce nom")
-    elif len(workflow) == 0:
+    elif len(workflows) == 0:
         raise ValueError("Pas de Workflow avec ce nom")
-    return workflow[0]
-    
+    wf_id = workflows[0][u'id']
+
+    return gi.workflows.show_workflow(wf_id)
+
 
 def specific_download_dataset(gi, dataset, id, idhisto, key, file_path, use_default_filename=True, verify=True):
     download_url = 'api/histories/' + idhisto+ '/contents/' + id + '/display?to_ext=' + dataset['file_ext'] +'&hda_ldda=' + dataset['hda_ldda'] + '&key=' + key
@@ -77,12 +85,14 @@ def specific_download_dataset(gi, dataset, id, idhisto, key, file_path, use_defa
         if use_default_filename:
             try:
                 # First try to get the filename from the response headers
-                # We expect tokens 'filename' '=' to be followed by the quoted filename
+                # We expect tokens 'filename' '=' to be followed by the
+                # quoted filename
                 tokens = [x for x in shlex.shlex(r.headers['content-type'], posix=True)]
                 header_filepath = tokens[tokens.index('filename') + 2]
                 filename = os.path.basename(header_filepath)
             except (ValueError, IndexError):
-                # If the filename was not in the header, build a useable filename ourselves.
+                # If the filename was not in the header, build a useable
+                # filename ourselves.
                 filename = dataset['name'] + '.' + dataset['file_ext']
             file_local_path = os.path.join(file_path, filename)
         else:
@@ -106,7 +116,7 @@ if __name__ == "__main__":
     parser.add_argument("-u", "--galaxy_url", action="store", help="url of galaxy")
     parser.add_argument("-m", "--my_histories", help="list yours histories", action = 'store_true')
     parser.add_argument("-hi", "--history_list", help="list files of a history", action = 'store_true')
-    parser.add_argument("-n", "--name", action="store", help="name of galaxy history")
+    parser.add_argument("-n", "--hist_name", action="store", help="name of galaxy history")
     parser.add_argument("-w", "--name_workflow", action="store", help="id of your workflow")
     parser.add_argument("-d", "--download_files", help="download files")
     parser.add_argument("-f", "--filesid", nargs="+", help="file id to download")
@@ -120,14 +130,14 @@ if __name__ == "__main__":
         pprint.pprint(gi.histories.get_histories())
         #liste_historyfiles(args.galaxy_url, args.api_key)
     elif args.history_list:
-        if args.name:
-            files, idhisto = liste_historyfiles(args.api_key, args.galaxy_url, args.name)
+        if args.hist_name:
+            files, idhisto = liste_historyfiles(args.api_key, args.galaxy_url, args.hist_name)
             for file in files:
                 print "FILENAME: {0:80} FILE_ID: {1} HISTORY_ID: {2}".format(files[file], file, idhisto)
         else:
             print "the -hi option need a history name"
     elif args.name_workflow:
-        run_workflow(args.api_key, args.galaxy_url, args.name_workflow, args.name, args.filesid)
+        run_workflow(args.api_key, args.galaxy_url, args.name_workflow, args.hist_name)#, args.filesid)
     elif args.download_files:
         if args.fileid:
             downloadfile(args.api_key, args.galaxy_url, args.fileid, idhisto, args.path)
